7d59783e5596a70387989635269cd3aed029df60
omelet1 <omelet1@omelet1ui-MacBook-Neo.local>
2026-07-26 05:58:08 +0900

    for문 완성

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use rand::prelude::*;
 
 fn main() {
-
     let mut rng: ThreadRng = rand::rng();
 
     // let password: String = (0..16).map(|_| rng.random_range(33u8..=126u8) as char).collect();
 
-    let char_range = 33u8 as char..=126u8 as char; 
-    let mut vec_char:Vec<char> = Vec::new();
+    let char_range = 33u8 as char..=126u8 as char;
+    let mut vec_char: Vec<char> = Vec::new();
 
     for i in char_range {
         vec_char.push(i);
@@ -16,8 +15,17 @@ fn main() {
     for (i, value) in vec_char.iter().enumerate() {
         println!("i : {}, value: {}", i, value);
     }
+
+    let mut password = String::new();
     
+    for _ in 0..16 {
+        let idx = rng.random_range(0..vec_char.len());
+        password.push(vec_char[idx]);
+    }
 
-
+    println!("password : {}", password);
 }
 
+fn generate_password() {
+    
+}

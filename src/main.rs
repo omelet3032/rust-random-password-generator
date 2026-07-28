b1cdad03ec058901124a981d5fb8b1d8596d7749
omelet1 <omelet1@omelet1ui-MacBook-Neo.local>
2026-07-29 05:59:52 +0900

    generate_password() 구현중

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,42 @@
 use rand::prelude::*;
 
 fn main() {
-    let mut rng: ThreadRng = rand::rng();
 
     // let password: String = (0..16).map(|_| rng.random_range(33u8..=126u8) as char).collect();
 
     let char_range = 33u8 as char..=126u8 as char;
     let mut vec_char: Vec<char> = Vec::new();
+    let password = String::new();
 
     for i in char_range {
         vec_char.push(i);
     }
 
-    for (i, value) in vec_char.iter().enumerate() {
-        println!("i : {}, value: {}", i, value);
-    }
-
-    let mut password = String::new();
+    let length: usize = 16;
     
-    for _ in 0..16 {
-        let idx = rng.random_range(0..vec_char.len());
-        password.push(vec_char[idx]);
-    }
-
-    println!("password : {}", password);
+    let result = generate_password(length);
+    println!("result : {} ", result);
 }
 
-fn generate_password() {
+fn generate_password(length:usize) -> String {
+
+    let mut rng: ThreadRng = rand::rng();
+
+    let vec_char: Vec<char> = (33u8..=126u8).map(|c| c as char).collect();
+
+    // let mut password = String::new();
     
+    let password: String = (0..length).map(|_| {
+        let idx = rng.random_range(0..vec_char.len());
+        vec_char[idx]
+    }).collect();
+
+    password
+
+
+    // for _ in 0..16 {
+    //     let idx = rng.random_range(0..vec_char.len());
+    //     password.push(vec_char[idx]);
+    // }
+
 }
